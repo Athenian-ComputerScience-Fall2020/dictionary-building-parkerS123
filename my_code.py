16e869f06723f915681cb0f7102dbf520207420b
parkerS123 <23pschwantes@athenian.org>
2020-10-21 19:35:18 +0000

    finished

--- my_code.py
+++ my_code.py
@@ -1,3 +1,19 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
 #  
 
+sports_players = {}
+position = 'start'
+
+while position != 'break':
+    position = input("Enter a football positon or enter break to stop: ")
+    if position == 'break':
+        break
+    player = input("Enter a player that plays that position: ")
+    sports_players[position] = player
+
+for x, y in sports_players.items():
+    print(x, y)
+        #sports_players[x] = y
+        #print({x: y})
+
+print(sports_players)
